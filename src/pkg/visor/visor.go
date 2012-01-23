3de4eac6027b31ea35eb7255d258a578d612e265
Alexander Simmerl <alx@soundcloud.com>
2012-01-23 17:20:53 +0100

    Provide default addr for coordinator

--- src/pkg/visor/visor.go
+++ src/pkg/visor/visor.go
@@ -5,15 +5,17 @@ import (
 	"net"
 )
 
+const DEFAULT_ADDR string = "localhost:8046"
+
 type ProcessType string
 type State string
 
 func Dial(addr string) (*Client, error) {
 	conn, err := doozer.Dial(addr)
-
 	if err != nil {
 		return nil, err
 	}
+
 	tcpaddr, err := net.ResolveTCPAddr("tcp", addr)
 
 	return &Client{tcpaddr, conn, "/"}, nil
