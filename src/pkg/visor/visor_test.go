d3c8e30986587a2b35653281b2c5870b3f7ed0c4
Alexander Simmerl <alx@soundcloud.com>
2012-01-23 17:42:46 +0100

    Test invalid addr

--- src/pkg/visor/visor_test.go
+++ src/pkg/visor/visor_test.go
@@ -4,9 +4,16 @@ import (
 	"testing"
 )
 
-func TestDial(t *testing.T) {
+func TestDialWithDefaultAddr(t *testing.T) {
 	_, err := Dial(DEFAULT_ADDR)
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+func TestDialWithInvalidAddr(t *testing.T) {
+  _, err := Dial("foo.bar:123:876")
+  if err == nil {
+    t.Error("Dialed with invalid addr")
+  }
 }
