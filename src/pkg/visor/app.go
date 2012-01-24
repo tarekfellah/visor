c395fd0cf8b286e222e205b38c3377d9158f6149
Alexander Simmerl <alx@soundcloud.com>
2012-01-24 17:41:56 +0100

    Implement app registration

--- src/pkg/visor/app.go
+++ src/pkg/visor/app.go
@@ -1,5 +1,9 @@
 package visor
 
+import (
+	"strings"
+)
+
 type App struct {
 	Name    string
 	RepoUrl string
@@ -15,11 +19,33 @@ func (a *App) RegisterRevision(rev string) (*Revision, error) {
 func (a *App) UnregisterRevision(r *Revision) error {
 	return nil
 }
-func (a *App) Register() error {
-	return nil
+func (a *App) Register(c *Client) (err error) {
+	exists, err := c.Exists(a.Path())
+	if err != nil {
+		return
+	}
+	if exists {
+		return ErrAppConflict
+	}
+
+	rev, err := a.setPath(c, "repo-url", a.RepoUrl)
+	if err != nil {
+		return
+	}
+	c.Rev = rev
+
+	rev, err = a.setPath(c, "stack", string(a.Stack))
+	if err != nil {
+		return
+	}
+	c.Rev = rev
+
+	return
 }
-func (a *App) Unregister() error {
-	return nil
+func (a *App) Unregister(c *Client) error {
+	path := strings.Join([]string{"/apps", a.Name, "repo-url"}, "/")
+
+	return c.Deldir(path, c.Rev)
 }
 func (a *App) EnvironmentVariables() (*map[string]string, error) {
 	return nil, nil
@@ -29,4 +55,17 @@ func (a *App) GetEnvironmentVariable(k string) (string, error) {
 }
 func (a *App) SetEnvironmentVariable(k string, v string) error {
 	return nil
+}
+func (a *App) String() string {
+	return "App{\"" + a.Name + "\"}"
+}
+
+func (a *App) Path() (p string) {
+	return "/apps/" + a.Name
+}
+
+func (a *App) setPath(c *Client, k string, v string) (int64, error) {
+	path := strings.Join([]string{a.Path(), k}, "/")
+
+	return c.Conn.Set(path, c.Rev, []byte(v))
 }
