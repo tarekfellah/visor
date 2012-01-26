36c53b03484d271f3222e319677d67a1f929de24
Alexander Simmerl <alx@soundcloud.com>
2012-01-26 20:32:47 +0100

    Add doc for Ticket

--- src/pkg/visor/ticket.go
+++ src/pkg/visor/ticket.go
@@ -6,6 +6,7 @@ import (
 	"net"
 )
 
+// Ticket carries instructions to start and stop Instances.
 type Ticket struct {
 	Type        TicketType
 	App         *App
@@ -14,6 +15,8 @@ type Ticket struct {
 	Addr        net.TCPAddr
 	Source      *doozer.Event
 }
+
+// TicketType identifies different operations.
 type TicketType int
 
 const (
@@ -21,15 +24,22 @@ const (
 	T_STOP
 )
 
+// Claim locks the Ticket to the passed host
 func (t *Ticket) Claim() error {
 	return nil
 }
+
+// Unclaim removes the lock applied by Claim of the Ticket.
 func (t *Ticket) Unclaim() error {
 	return nil
 }
+
+// Done marks the Ticket as done/solved in the registry.
 func (t *Ticket) Done() error {
 	return nil
 }
+
+// String returns the Go-syntax representation of Ticket
 func (t *Ticket) String() string {
 	return fmt.Sprintf("%#v", t)
 }
