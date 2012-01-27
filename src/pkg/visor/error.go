42370461e4f00dded643f9f49519e1c695708deb
Alexander Simmerl <alx@soundcloud.com>
2012-01-27 14:03:49 +0100

    Add error for bad operations

--- src/pkg/visor/error.go
+++ src/pkg/visor/error.go
@@ -8,6 +8,7 @@ var (
 	ErrKeyConflict   = errors.New("key is already set")
 	ErrKeyNotFound   = errors.New("key not found")
 	ErrTicketClaimed = errors.New("ticket already claimed")
+	ErrUnauthorized  = errors.New("operation is not permitted")
 )
 
 type Error struct{}
