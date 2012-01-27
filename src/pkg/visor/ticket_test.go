3c4e4beccf15895cf98e93740e55703fd7c703b5
Alexander Simmerl <alx@soundcloud.com>
2012-01-27 13:35:22 +0100

    Use builtin hostname call

--- src/pkg/visor/ticket_test.go
+++ src/pkg/visor/ticket_test.go
@@ -1,15 +1,20 @@
 package visor
 
 import (
+  "os"
 	"strconv"
 	"testing"
 )
 
-func ticketSetup() (c *Client) {
+func ticketSetup() (c *Client, hostname string) {
 	c, err := Dial(DEFAULT_ADDR, DEFAULT_ROOT)
 	if err != nil {
 		panic(err)
 	}
+  hostname, err = os.Hostname()
+  if err != nil {
+    panic(err)
+  }
 
 	c.Del("tickets")
 
@@ -37,7 +42,7 @@ func TestNewTicket(t *testing.T) {
 }
 
 func TestClaim(t *testing.T) {
-	c := ticketSetup()
+	c, host := ticketSetup()
 	id := c.rev
 	op := "claim abcd123 test start"
 	ticket := &Ticket{Id: id, AppName: "claim", RevisionName: "abcd123", ProcessType: "test", Op: 0}
@@ -47,20 +52,21 @@ func TestClaim(t *testing.T) {
 		t.Error(err)
 	}
 
-	err = ticket.Claim(c, "localhost")
+	err = ticket.Claim(c, host)
 	if err != nil {
 		t.Error(err)
 	}
+
 
 	body, err := c.Get("tickets/" + strconv.FormatInt(id, 10) + "/claimed")
 	if err != nil {
 		t.Error(err)
 	}
-	if body != "localhost" {
+	if body != host {
 		t.Error("Ticket not claimed")
 	}
 
-	err = ticket.Claim(c, "localhost")
+	err = ticket.Claim(c, host)
 	if err != ErrTicketClaimed {
 		t.Error("Ticket claimed twice")
 	}
