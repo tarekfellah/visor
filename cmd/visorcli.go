618ebc7c2766093fbb6ff689737118131df6e371
Patrick Ellis <patrick@soundcloud.com>
2012-06-08 17:14:32 +0300

    Grammar Nazi II: Revenge of the Grammar Nazi

--- cmd/visorcli.go
+++ cmd/visorcli.go
@@ -131,7 +131,7 @@ func main() {
 						},
 					},
 					"unregister": {
-						"unregister / delete a new application, and all it's revisions and stop all running instances",
+						"unregister / delete a new application, and all its revisions and stop all running instances",
 						getopt.Definitions{
 							{"name", "name of the new app", getopt.IsArg | getopt.Required, ""},
 						},
