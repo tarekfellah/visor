2ccaf639157412fe65906fd8a97c8537e0f3ce32
cloudhead <alexis@cloudhead.io>
2012-07-05 19:12:43 +0200

    Remove all references to heartbeats & max-restarts

--- proctype.go
+++ proctype.go
@@ -17,33 +17,14 @@ type ProcType struct {
 	Snapshot
 	Name        ProcessName
 	App         *App
-	Heartbeat   *Heartbeat
 	MaxRestarts int
 	Port        int
 }
 
-type Heartbeat struct {
-	Interval     int
-	Treshold     int
-	InitialDelay int
-}
-
 const PROCS_PATH = "procs"
 
-var (
-	HEARTBEAT_INTERVAL      = 30
-	HEARTBEAT_TRESHOLD      = 2
-	HEARTBEAT_INITIAL_DELAY = 1
-)
-
-var DEFAULT_HEARTBEAT = &Heartbeat{
-	Interval:     HEARTBEAT_INTERVAL,
-	Treshold:     HEARTBEAT_TRESHOLD,
-	InitialDelay: HEARTBEAT_INITIAL_DELAY,
-}
-
 func NewProcType(app *App, name ProcessName, s Snapshot) *ProcType {
-	return &ProcType{Name: name, App: app, Snapshot: s, Heartbeat: DEFAULT_HEARTBEAT}
+	return &ProcType{Name: name, App: app, Snapshot: s}
 }
 
 func (p *ProcType) createSnapshot(rev int64) Snapshotable {
@@ -73,19 +54,9 @@ func (p *ProcType) Register() (ptype *ProcType, err error) {
 		return nil, errors.New(fmt.Sprintf("couldn't claim port: %s", err.Error()))
 	}
 
-	if p.Heartbeat == nil {
-		p.Heartbeat = DEFAULT_HEARTBEAT
-	}
+	port := &File{p.Snapshot, p.Path() + "/port", p.Port, new(IntCodec)}
 
-	attrs := &File{p.Snapshot, p.Path() + "/attrs", map[string]int{
-		"heartbeat-interval":      p.Heartbeat.Interval,
-		"heartbeat-treshold":      p.Heartbeat.Treshold,
-		"heartbeat-initial-delay": p.Heartbeat.InitialDelay,
-		"max-restarts":            p.MaxRestarts,
-		"port":                    p.Port,
-	}, new(JSONCodec)}
-
-	attrs, err = attrs.Create()
+	port, err = port.Create()
 	if err != nil {
 		return p, err
 	}
@@ -155,23 +126,16 @@ func (p *ProcType) GetInstanceInfos() (ins []*InstanceInfo, err error) {
 func GetProcType(s Snapshot, app *App, name ProcessName) (p *ProcType, err error) {
 	path := path.Join(app.Path(), PROCS_PATH, string(name))
 
-	f, err := Get(s, path+"/attrs", new(JSONCodec))
+	port, err := Get(s, path+"/port", new(IntCodec))
 	if err != nil {
 		return
 	}
-	value := f.Value.(map[string]interface{})
 
 	p = &ProcType{
-		Name:        name,
-		Snapshot:    s,
-		App:         app,
-		Port:        int(value["port"].(float64)),
-		MaxRestarts: int(value["max-restarts"].(float64)),
-		Heartbeat: &Heartbeat{
-			Interval:     int(value["heartbeat-interval"].(float64)),
-			Treshold:     int(value["heartbeat-treshold"].(float64)),
-			InitialDelay: int(value["heartbeat-initial-delay"].(float64)),
-		},
+		Name:     name,
+		Snapshot: s,
+		App:      app,
+		Port:     port.Value.(int),
 	}
 	return
 }
