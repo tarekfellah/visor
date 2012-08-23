13fc4bcd6803dcee95e31a8cf7d2d61a30057c8c
Daniel Bornkessel <daniel@soundcloud.com>
2012-08-23 10:34:01 +0200

    Check if mercurial is working correctly and abort if not the case
    Quite some users had failing mercurial installation. While this is
    in theory out of scope for the visor installation it causes a lot
    of trouble for users not being able to install visor (as a dependency
    of bazooka-cli).
    
    Waiting for feedback if the `hg --version` is enough to trigger the
    problem as I can't recreate the problem here

--- visor.rb
+++ visor.rb
@@ -14,6 +14,25 @@ class Visor < Formula
     rescue
       system "brew install hg"
     end
+
+    begin
+      system("hg --version")
+    rescue
+      hg_path = `which hg`
+      puts <<-EOF
+
+Your mercurial installation is broken. Please delete your current mercurial installation
+and reinstall with brew by executing:
+
+  sudo mv #{hg_path.chomp} /tmp
+  brew install --force mercurial || brew update --force mercurial
+
+
+
+      EOF
+
+      exit 1
+    end
     ENV['GOPATH'] = buildpath
     ENV['GOBIN'] = "#{prefix}/bin"
     system "make"
