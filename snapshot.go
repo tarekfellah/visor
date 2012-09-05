d1a683b1b32dce48a4dc19f6bbea8814e95df913
cloudhead <alexis@cloudhead.io>
2012-09-05 11:42:07 +0200

    Update doc & re-order functions

--- snapshot.go
+++ snapshot.go
@@ -61,71 +61,6 @@ func DialUri(uri string, root string) (s Snapshot, err error) {
 	return
 }
 
-// Exists checks if the specified path exists at this snapshot's revision
-func (s Snapshot) exists(path string) (bool, int64, error) {
-	return s.conn.ExistsRev(path, &s.Rev)
-}
-
-// Get returns the value at the specified path, at this snapshot's revision
-func (s Snapshot) get(path string) (string, int64, error) {
-	val, rev, err := s.getBytes(path)
-	return string(val), rev, err
-}
-
-// GetFile returns the value at the specified path as a file, at this snapshot's revision
-func (s Snapshot) getFile(path string, codec Codec) (*File, error) {
-	return Get(s, path, codec)
-}
-
-// GetBytes returns the value at the specified path, at this snapshot's revision
-func (s Snapshot) getBytes(path string) ([]byte, int64, error) {
-	return s.conn.Get(path, &s.Rev)
-}
-
-// Getdir returns the list of files in the specified directory, at this snapshot's revision
-func (s Snapshot) getdir(path string) ([]string, error) {
-	return s.conn.Getdir(path, s.Rev)
-}
-
-// Set sets the specfied path's body to the passed value, at this snapshot's revision
-func (s Snapshot) set(path string, val string) (Snapshot, error) {
-	return s.setBytes(path, []byte(val))
-}
-
-// SetBytes sets the specfied path's body to the passed value, at this snapshot's revision
-func (s Snapshot) setBytes(path string, val []byte) (Snapshot, error) {
-	rev, err := s.conn.Set(path, s.Rev, val)
-	if err != nil {
-		return s, err
-	}
-	return s.FastForward(rev), err
-}
-
-// Del deletes the file at the specified path, at this snapshot's revision
-func (s Snapshot) del(path string) error {
-	return s.conn.Del(path, s.Rev)
-}
-
-// Update checks if the specified path exists, and if so, does a (*Snapshot).Set with the passed value.
-func (s Snapshot) update(path string, val string) (Snapshot, error) {
-	exists, _, err := s.exists(path)
-	if err != nil {
-		return s, err
-	}
-	if !exists {
-		return s, NewError(ErrNoEnt, fmt.Sprintf("path '%s' does not exist at %d", path, s.Rev))
-	}
-	return s.set(path, val)
-}
-
-func (s Snapshot) createSnapshot(rev int64) Snapshotable {
-	return Snapshot{rev, s.conn}
-}
-
-func (s Snapshot) FastForward(rev int64) (ns Snapshot) {
-	return s.fastForward(s, rev).(Snapshot)
-}
-
 // NOTE: This method does not check whether or not the scale target exists.
 // A scale of `0` will be returned if any of the path components are missing.
 // This is to avoid having to set the /apps/<app>/revs/<rev>/scale/<proc> paths to 0
@@ -155,6 +90,77 @@ func (s Snapshot) GetScale(app string, revision string, processName string) (sca
 func (s Snapshot) SetScale(app string, revision string, processName string, factor int) (s1 Snapshot, err error) {
 	path := path.Join(APPS_PATH, app, REVS_PATH, revision, SCALE_PATH, processName)
 	return s.set(path, strconv.Itoa(factor))
+}
+
+
+// Getuid returns a unique ID from the coordinator
+func Getuid(s Snapshot) (int64, error) {
+	return s.conn.Set(UID_PATH, -1, []byte{})
+}
+
+// exists checks if the specified path exists at this snapshot's revision
+func (s Snapshot) exists(path string) (bool, int64, error) {
+	return s.conn.ExistsRev(path, &s.Rev)
+}
+
+// get returns the value at the specified path, at this snapshot's revision
+func (s Snapshot) get(path string) (string, int64, error) {
+	val, rev, err := s.getBytes(path)
+	return string(val), rev, err
+}
+
+// getFile returns the value at the specified path as a file, at this snapshot's revision
+func (s Snapshot) getFile(path string, codec Codec) (*File, error) {
+	return Get(s, path, codec)
+}
+
+// getBytes returns the value at the specified path, at this snapshot's revision
+func (s Snapshot) getBytes(path string) ([]byte, int64, error) {
+	return s.conn.Get(path, &s.Rev)
+}
+
+// getdir returns the list of files in the specified directory, at this snapshot's revision
+func (s Snapshot) getdir(path string) ([]string, error) {
+	return s.conn.Getdir(path, s.Rev)
+}
+
+// set sets the specfied path's body to the passed value, at this snapshot's revision
+func (s Snapshot) set(path string, val string) (Snapshot, error) {
+	return s.setBytes(path, []byte(val))
+}
+
+// setBytes sets the specfied path's body to the passed value, at this snapshot's revision
+func (s Snapshot) setBytes(path string, val []byte) (Snapshot, error) {
+	rev, err := s.conn.Set(path, s.Rev, val)
+	if err != nil {
+		return s, err
+	}
+	return s.FastForward(rev), err
+}
+
+// del deletes the file at the specified path, at this snapshot's revision
+func (s Snapshot) del(path string) error {
+	return s.conn.Del(path, s.Rev)
+}
+
+// update checks if the specified path exists, and if so, does a (*Snapshot).Set with the passed value.
+func (s Snapshot) update(path string, val string) (Snapshot, error) {
+	exists, _, err := s.exists(path)
+	if err != nil {
+		return s, err
+	}
+	if !exists {
+		return s, NewError(ErrNoEnt, fmt.Sprintf("path '%s' does not exist at %d", path, s.Rev))
+	}
+	return s.set(path, val)
+}
+
+func (s Snapshot) createSnapshot(rev int64) Snapshotable {
+	return Snapshot{rev, s.conn}
+}
+
+func (s Snapshot) FastForward(rev int64) (ns Snapshot) {
+	return s.fastForward(s, rev).(Snapshot)
 }
 
 // fastForward either calls *createSnapshot* on *obj* or returns *obj* if it
@@ -205,9 +211,4 @@ func GetLatest(s Snapshot, path string, codec Codec) (file *File, err error) {
 	file = &File{Path: path, Value: value, Codec: codec, Snapshot: s.FastForward(rev)}
 
 	return
-}
-
-// Getuid returns a unique ID from the coordinator
-func Getuid(s Snapshot) (int64, error) {
-	return s.conn.Set(UID_PATH, -1, []byte{})
 }
