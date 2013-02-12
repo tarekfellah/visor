c4ce34648689c2c1ad850fa7b126b05a90bbf2df
cloudhead <alexis@cloudhead.io>
2013-02-12 16:11:21 +0100

    Implement Runner type, remove Endpoint/Service
    Discovery will no longer take place inside of bazooka.
    We implement a Runner type as a substitute to Endpoint, to
    register our service runners.

---
+++ runner.go
@@ -0,0 +1,92 @@
+// Copyright (c) 2012, SoundCloud Ltd.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// Source code and contact info at http://github.com/soundcloud/visor
+
+package visor
+
+import (
+	"fmt"
+	"path"
+	"strconv"
+	"strings"
+)
+
+const runnersPath = "runners"
+
+type Runner struct {
+	Dir        dir
+	Addr       string
+	InstanceId int64
+}
+
+func NewRunner(addr string, instanceId int64, s Snapshot) *Runner {
+	return &Runner{
+		Dir:        dir{s, runnerPath(addr)},
+		Addr:       addr,
+		InstanceId: instanceId,
+	}
+}
+
+func (r *Runner) Register() (*Runner, error) {
+	f, err := createFile(r.Dir.Snapshot, r.Dir.Name, []string{strconv.FormatInt(r.InstanceId, 10)}, new(listCodec))
+	if err != nil {
+		return nil, err
+	}
+	return r.FastForward(f.Snapshot.Rev), nil
+}
+
+func (r *Runner) createSnapshot(rev int64) snapshotable {
+	tmp := *r
+	tmp.Dir.Snapshot = Snapshot{rev, r.Dir.Snapshot.conn}
+	return &tmp
+}
+
+// FastForward advances the runner in time. It returns
+// a new instance of Runner with the supplied revision.
+func (r *Runner) FastForward(rev int64) *Runner {
+	return r.Dir.Snapshot.fastForward(r, rev).(*Runner)
+}
+
+func RunnersByHost(s Snapshot, host string) (runners []*Runner, err error) {
+	ids, err := s.getdir(path.Join(runnersPath, host))
+	if err != nil {
+		return
+	}
+	ch, errch := getSnapshotables(ids, func(id string) (snapshotable, error) {
+		return GetRunner(s, id)
+	})
+	for i := 0; i < len(ids); i++ {
+		select {
+		case r := <-ch:
+			runners = append(runners, r.(*Runner))
+		case e := <-errch:
+			if err == nil {
+				err = e
+			} else {
+				err = fmt.Errorf("%s\n%s", err, e)
+			}
+		}
+	}
+	return
+}
+
+func GetRunner(s Snapshot, addr string) (*Runner, error) {
+	f, err := s.getFile(runnerPath(addr), new(listCodec))
+	if err != nil {
+		return nil, err
+	}
+	data := f.Value.([]string)
+	insIdStr := data[0]
+	insId, err := parseInstanceId(insIdStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewRunner(addr, insId, s), nil
+}
+
+func runnerPath(addr string) string {
+	parts := strings.Split(addr, ":")
+	return path.Join(runnersPath, parts[0], parts[1])
+}
