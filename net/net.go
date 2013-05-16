b9c6a5f48498d85a400c9ca7f3e8ce2178470395
Alexander Simmerl <alx@soundcloud.com>
2013-05-16 17:17:04 +0200

    Force timeout in net package
    The net package used by the runner didn't force timeouts after the
    connect succeeded. This broke assumptions when used together with
    the Runner type and Connect calls hung indefinitely.

--- net/net.go
+++ net/net.go
@@ -13,5 +13,43 @@ type Network interface {
 type Net struct{}
 
 func (n *Net) Dial(addr string) (io.ReadWriteCloser, error) {
-	return _net.DialTimeout("tcp", addr, time.Second)
+	c, err := _net.DialTimeout("tcp", addr, time.Second)
+
+	if err != nil {
+		return c, err
+	}
+
+	return &conn{c}, nil
+}
+
+type conn struct {
+	_net.Conn
+}
+
+func (c *conn) Write(b []byte) (int, error) {
+	if err := c.Conn.SetWriteDeadline(time.Now().Add(time.Second)); err != nil {
+		return 0, err
+	}
+
+	n, err := c.Conn.Write(b)
+
+	if err == nil {
+		c.Conn.SetWriteDeadline(time.Time{})
+	}
+
+	return n, err
+}
+
+func (c *conn) Read(b []byte) (int, error) {
+	if err := c.Conn.SetReadDeadline(time.Now().Add(time.Second)); err != nil {
+		return 0, err
+	}
+
+	n, err := c.Conn.Read(b)
+
+	if err == nil {
+		c.Conn.SetReadDeadline(time.Time{})
+	}
+
+	return n, err
 }
